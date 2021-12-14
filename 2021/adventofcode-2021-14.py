78ffdb44f57aa0ffa0d85598b5ec48bcc58ddb40
Ankit Gupta <ankitsumitg@gmail.com>
2021-12-14 22:04:50 +0530

    Added 14th question of 2021 adventofcode

---
+++ 2021/adventofcode-2021-14.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+STEPS = 40
+d = defaultdict(int)
+joiners = defaultdict(int)
+
+with open('input14', 'r') as f:
+    s, waste, *conn = f.read().splitlines()
+    conn = {k: v for k, v in map(lambda x: x.split(' -> '), conn)}
+    for i, j in zip(s, s[1:]):
+        joiners[i + j] += 1
+    for k in s:
+        d[k] += 1
+for _ in range(STEPS):
+    for key, val in joiners.copy().items():
+        link = conn[key]
+        joiners[key] -= val
+        joiners[key[0] + link] += val
+        joiners[link + key[1]] += val
+        d[link] += val
+    print(f'Step {_ + 1}: {max(d.values()) - min(d.values())}')
